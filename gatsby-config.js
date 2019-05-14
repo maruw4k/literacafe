901b7aabc7c7e0d4f394911c307488fa1c223a7b
Marek Krzeszowiec <marek.krzeszowiec@alterpage.pl>
2019-05-14 23:32:51 +0200

    Edit default site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `LiteraCafe`,
+    description: `Strona kawiarni LiteraCafe`,
+    author: `@maruw4k`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
