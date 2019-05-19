b836268d1adb57a439c8e40bdae0c34d19fbf59c
Marek Krzeszowiec <marek.krzeszowiec@alterpage.pl>
2019-05-20 00:56:57 +0200

    Create CSS helper (SSOT)
    To have Single Source of Truth

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,22 @@
+export const theme = {
+  font: {
+    size: {
+      xs: '1.4rem',
+      s: '2.6rem',
+      m: '4.2rem',
+      l: '5rem',
+      xl: '7.2rem',
+      xxl: '9.4rem',
+    },
+    weight: {
+      light: '300',
+      regular: '400',
+      bold: '700',
+    },
+  },
+  mq: {
+    tablet: `@media (min-width: 768px)`,
+    desktop: `@media (min-width: 1024px)`,
+    huge: `@media(min-width: 1440px)`,
+  },
+};
